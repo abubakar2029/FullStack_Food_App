245bb25efde59b7daf8f4dee163b1ab2948f2ca6
Abu Bakar <abubakarzafar2029@gmail.com>
2024-06-09 23:07:56 +0500

    Stop adding .env file

--- foodapp/prisma/seed.ts
+++ foodapp/prisma/seed.ts
@@ -1,52 +1,51 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function createRecords() {
   try {
-    // Create users first
     const user1 = await prisma.user.create({
       data: {
-        fullName: 'Asad Ali',
-        password: 'password2',
-        email: 'asad_ali@foodapp.com',
-        role: 'seller',
+        fullName: "Asad Ali",
+        password: "password2",
+        email: "asad_ali@foodapp.com",
+        role: "seller",
       },
     });
 
     const user2 = await prisma.user.create({
       data: {
-        fullName: 'Zia Khan',
-        password: 'password3',
-        email: 'zia_khan@foodapp.com',
-        role: 'seller',
+        fullName: "Zia Khan",
+        password: "password3",
+        email: "zia_khan@foodapp.com",
+        role: "seller",
       },
     });
 
     // Use user IDs to create related records
     await prisma.seller.create({
       data: {
-        id: user1.id, // Use the user ID
-        storeName: 'Best Bites',
-        state: 'Punjab',
-        city: 'Lahore',
-        streetAddress: '789 Gulberg Rd',
+        id: user1.id,
+        storeName: "Best Bites",
+        state: "Punjab",
+        city: "Lahore",
+        streetAddress: "789 Gulberg Rd",
       },
     });
 
     await prisma.seller.create({
       data: {
         id: user2.id, // Use the user ID
-        storeName: 'Yummy Foods',
-        state: 'Sindh',
-        city: 'Karachi',
-        streetAddress: '321 Saddar Ln',
+        storeName: "Yummy Foods",
+        state: "Sindh",
+        city: "Karachi",
+        streetAddress: "321 Saddar Ln",
       },
     });
 
-    console.log('Records created');
+    console.log("Records created");
   } catch (error) {
-    console.error('Error creating records:', error);
+    console.error("Error creating records:", error);
   } finally {
     await prisma.$disconnect();
   }
